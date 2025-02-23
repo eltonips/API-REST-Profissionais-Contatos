ca7b90d93ffdb279ab2434fef9bbc347e4598dcf
eltonips <42722086+eltonips@users.noreply.github.com>
2025-02-22 23:38:18 -0300

    Criado projeto

---
+++ src/main/java/com/project/ApiRestProfCont/ApiRestProfContApplication.java
@@ -0,0 +1,13 @@
+package com.project.ApiRestProfCont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProfContApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProfContApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ApiRestProfCont/ApiRestProfContApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ApiRestProfCont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProfContApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
